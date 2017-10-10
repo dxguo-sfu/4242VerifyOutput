62841a02725dcac58dbd7081c532615af98c9edf
cwhite <whitecooper55@gmail.com>
2017-10-10 16:48:47 -0700

    Update display to handle digits greater than 9

--- src/DisplayResults.java
+++ src/DisplayResults.java
@@ -6,14 +6,18 @@ import java.util.List;
 public class DisplayResults {
     public DisplayResults(List<Banner> allBanners, List<Banner> faultyBanners) {
         System.out.println(faultyBanners.size() + " out of " + allBanners.size() + " are faulty.\n");
-        if(faultyBanners.size() > 0) {
+        if (faultyBanners.size() > 0) {
             System.out.println("Faulty banners: ");
             int i = 1;
+            String padding = "     ";
             for (Banner banner : faultyBanners) {
                 System.out.println(i + ". Config: " + banner.getConfiguratorInput());
-                System.out.println("     Cart: " + banner.getCartInput() + "\n");
-                i++;
+                System.out.println(padding + "Cart: " + banner.getCartInput() + "\n");
+                if(Integer.toString(i).length() != Integer.toString(++i).length()){
+                    padding += " ";
+                }
             }
         }
     }
 }
+
